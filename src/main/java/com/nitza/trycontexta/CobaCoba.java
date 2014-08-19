8f346a4404fbe5e94a985a2ca8a8d2293f86480a
nitzaalfinas <nitzaalfinas@gmail.com>
2014-08-19 13:37:44 +0700

    first commit

---
+++ src/main/java/com/nitza/trycontexta/CobaCoba.java
@@ -0,0 +1,15 @@
+package com.nitza.trycontexta;
+
+public class CobaCoba {
+	
+	private String name;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String printHello() {
+		//System.out.println("spring 3 hello "+name);
+		return "spring 3 hello "+name;
+	}
+}
